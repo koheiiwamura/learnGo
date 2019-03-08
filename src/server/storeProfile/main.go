14e37a566de8314f6d0fbf8616d5e6beb514eaa9
岩村康平 <iwamurakouhei@iwamurakouheinoMacBook-Pro.local>
2019-03-09 08:22:09 +0900

    format go format

--- src/server/storeProfile/main.go
+++ src/server/storeProfile/main.go
@@ -9,6 +9,7 @@ import (
 	"github.com/julienschmidt/httprouter"
 )
 
+// Person は基本情報
 type Person struct {
 	Name         string   `json:"name"`
 	Age          int      `json:"age"`
@@ -16,9 +17,10 @@ type Person struct {
 	FavoriteFood []string `json:"favorite_foods"`
 }
 
+// Persons は生成したPersonを保存しておくmap
 var Persons map[string]Person = map[string]Person{}
 
-func profileDetail(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
+func getProfileDetail(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
 	name := p.ByName("name")
 	responsePerson, notFound := Persons[name]
 
@@ -33,14 +35,15 @@ func profileDetail(w http.ResponseWriter, r *http.Request, p httprouter.Params) 
 	w.Write([]byte(string(string(jsonBytes))))
 }
 
-func ProfileAdd(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
+// AddProfile はpersonを新規追加する関数
+func AddProfile(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
 
 	defer r.Body.Close()
 
 	var storePerson Person
 	// 構造体に当てはめる
-	var decode_err = json.NewDecoder(r.Body).Decode(&storePerson)
-	if decode_err != nil {
+	var err = json.NewDecoder(r.Body).Decode(&storePerson)
+	if err != nil {
 		http.Error(w, fmt.Sprintf("%d bad Request", http.StatusBadRequest), http.StatusBadRequest)
 		return
 	}
@@ -56,21 +59,21 @@ func ProfileAdd(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
 	log.Printf("\nnum of Persons: %d\n", len(Persons))
 	// 201を返答
 	w.WriteHeader(http.StatusCreated)
-	json_bytes, err := json.Marshal(storePerson)
+	jsonBytes, err := json.Marshal(storePerson)
 	if err != nil {
 		http.Error(w, fmt.Sprintf("%d Internal Server Error", http.StatusInternalServerError), http.StatusInternalServerError)
 		return
 	}
 	w.WriteHeader(http.StatusCreated)
-	w.Write([]byte(string(json_bytes)))
+	w.Write([]byte(string(jsonBytes)))
 }
 
 func main() {
 	router := httprouter.New() // HTTPルーターを初期化
 
-	router.POST("/Profile/add", ProfileAdd)
+	router.POST("/Profile/add", AddProfile)
 
-	router.GET("/Profile/:name", profileDetail)
+	router.GET("/Profile/:name", getProfileDetail)
 
 	// Webサーバーを8080ポートで立ち上げる
 	err := http.ListenAndServe(":8080", router)
